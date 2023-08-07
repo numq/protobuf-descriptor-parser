af7e1e756ac531086f04b7c7c8b6b0f9bbf6ac45
numq <numq.dev@gmail.com>
2023-08-07 14:58:37 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+}
+
+group = "io.github.numq"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+val protobufVersion = findProperty("protobuf.version")
+val schemaVersion = findProperty("schema.version")
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+    implementation("com.squareup.wire:wire-schema:$schemaVersion")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
